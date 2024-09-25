914cdb92062afae1ca4606629d3ef427e2f1394b
IvanRizal <ivanrizal36@gmail.com>
2024-09-25 13:04:23 +0700

    Praktikum WEB

---
+++ Praktikum10/time.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+</head>
+<body>
+    <h3>Time</h3>
+    <?php
+    date_default_timezone_set("Asia/Jakarta");  // Mengatur zona waktu ke Asia/Jakarta
+    echo date("h:i:sa");  // Menampilkan waktu dalam format jam:menit:detik AM/PM
+    ?>
+</body>
+</html>
